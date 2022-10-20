37ef7a11bde82bb1ccd874a0ca0eac6c9a460266
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2022-10-20 18:40:56 -0400

    intersection solution

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,16 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+  const map = new Map();
+  const set = new Set();
+
+  for (let e of a) {
+    map.set(e);
+  }
+  for (let e of b) {
+    if (map.has(e)) {
+      set.add(e);
+    }
+  }
+  return [...set];
+}
 
 module.exports = intersection;
